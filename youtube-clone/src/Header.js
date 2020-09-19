02b511841915722aae290a3d168ae70906adb502
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-19 15:24:19 -0400

    initialiazed the youtube app

---
+++ youtube-clone/src/Header.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  const [inputSearch, setInputSearch] = useState("");
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+            alt="YouTube Logo"
+          />
+        </Link>
+      </div>
+      <div className="header__input">
+        <input
+          onChange={(e) => setInputSearch(e.target.value)}
+          value={inputSearch}
+          placeholder="Search"
+          type="text"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
+      </div>
+      <div className="header__icons">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+        <Avatar
+          alt="Remy Sharp"
+          src="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+        />
+      </div>
+    </div>
+  );
+}
