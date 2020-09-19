5dad380cec901e04900ce46a7c7d94bcf8d7b13c
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-19 15:42:20 -0400

    added sidebarrow

--- youtube-clone/src/SidebarRow.js
+++ youtube-clone/src/SidebarRow.js
@@ -5,6 +5,7 @@ function SidebarRow({ selected, Icon, title }) {
   return (
     <div className="sidebarRow ${selected && 'selected'}">
       <Icon className="sidebarRow__icon" />
+      <h2 className="sidebarRow__title">{title}</h2>
     </div>
   );
 }
