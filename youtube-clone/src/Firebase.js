1b0dae1bfeede046001263a0196b868491e16f25
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-19 20:50:01 -0400

    styling for search page

---
+++ youtube-clone/src/Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAcrE7f--lZKvfsT0na6Zu9DywJe_p-OSc",
+  authDomain: "react-56ed5.firebaseapp.com",
+  databaseURL: "https://react-56ed5.firebaseio.com",
+  projectId: "react-56ed5",
+  storageBucket: "react-56ed5.appspot.com",
+  messagingSenderId: "756314391847",
+  appId: "1:756314391847:web:7b1ef73025c5ee21bb9502",
+  measurementId: "G-RCK40X0DK1",
+};
